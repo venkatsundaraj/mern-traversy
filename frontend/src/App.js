e75440aa82a4a9c7991379e8b5149fe937aa66c1
venkateshsundarasan@gmail.com <venkateshsundarasan@gmail.com>
2023-07-27 10:13:47 +0530

    dummy-commit112223

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,10 +20,6 @@ function App() {
             <Route exact path="/login" element={<Login/>}/>
             <Route exact path="/register" element={<Register/>}/>
             <Route exact path="/edit-goal/:id" element={<EditGoal/>}/>
-<<<<<<< HEAD
-            {/* <Route exact path="/test" element={<Test/>}/> */}
-=======
->>>>>>> 2506a8cafd0fd0378d46f5a4f339d93cf8f74dc3
             <Route path="*" element={<Dashboard/>}/>
           </Routes>
         </div>
